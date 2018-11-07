d32b21e38d3b19ba24125c950caeb9ea4441ca07
Luis Villena <luisvillena@gmail.com>
2018-11-07 11:43:26 -0300

    Use axios as requestor

--- src/webview_requestor.ts
+++ src/webview_requestor.ts
@@ -1,25 +1,17 @@
 import {AppAuthError} from "@openid/appauth/built/errors";
 import {Requestor} from "@openid/appauth/built/xhr";
-const request = require("browser-request");
+import axios from "axios";
 
 export class WebviewRequestor extends Requestor {
   public xhr<T>(settings: JQueryAjaxSettings): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-      request(
-          {
-            form: settings.data,
-            headers: settings.headers,
-            json: settings.dataType === "json" ? true : undefined,
-            method: settings.method,
-            url: settings.url,
-          },
-          (error: any, response: any, body: any) => {
-            if (response.statusCode !== 200) {
-              reject(new AppAuthError(response.statusMessage!));
-            } else {
-              resolve(body as T);
-            }
-          });
+    const request = {
+      data: settings.data,
+      headers: settings.headers,
+      method: settings.method || "GET",
+      url: settings.url,
+    };
+    return axios(request).then((response) => {
+      return (response.data);
     });
   }
 }
